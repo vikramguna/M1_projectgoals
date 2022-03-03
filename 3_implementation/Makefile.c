59157afd9fe017c91f2ae626dc5e0dc3f6c68d65
vikramguna <53256465+vikramguna@users.noreply.github.com>
2022-03-03 18:26:14 +0530

    Add files via upload

---
+++ 3_implementation/Makefile.c
@@ -0,0 +1,9 @@
+#target  : Dependencies
+#<TAB>commands
+
+Build : project_main.c contact management.c test_personal contact management. unity.c
+        gcc project_main,c contact management.c test_contact management.c unity.c -0 contact management.out
+Run : contact management.out
+            ./contact management.out
+Clean :
+       rm -rf *.0 *.out *.i *.s   
